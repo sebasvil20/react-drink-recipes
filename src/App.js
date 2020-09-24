2fdea0bc76d09c6d10b8e44de781faebd66b3b69
Sebastian villegas <sebasvil20@gmail.com>
2020-09-24 15:02:02 -0500

    Final commit, modal context added

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import CategoriasProvider from "./context/CategoriasContext";
 import RecetasProvider from "./context/RecetasContext";
+import ModalProvider from "./context/ModalContext";
 import ListaRecetas from "./components/ListaRecetas";
 
 function App() {
   return (
     <CategoriasProvider>
       <RecetasProvider>
-        <Header />
-        <div className="container mt-5">
-          <div className="row">
-            <Formulario />
+        <ModalProvider>
+          <Header />
+          <div className="container mt-5">
+            <div className="row">
+              <Formulario />
+            </div>
+            <ListaRecetas />
           </div>
-          <ListaRecetas />
-        </div>
+        </ModalProvider>
       </RecetasProvider>
     </CategoriasProvider>
   );
--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,6 +1,66 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { ModalContext } from "../context/ModalContext";
+import Modal from "@material-ui/core/Modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 
 const Receta = ({ receta }) => {
+  //Configuracion del modal de material-ui
+
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // extraer valores del context
+  const { inforeceta, guardarIdReceta, guardarReceta } = useContext(
+    ModalContext
+  );
+
+  //muestra y formatea los ingredientes
+  const mostrarIngredientes = (inforeceta) => {
+    let ingredientes = [];
+    for (let i = 1; i < 16; i++) {
+      if (inforeceta[`strIngredient${i}`]) {
+        ingredientes.push(
+          <li>
+            {inforeceta[`strIngredient${i}`]} {inforeceta[`strMeasure${i}`]}
+          </li>
+        );
+      }
+    }
+    console.log(ingredientes);
+    return ingredientes;
+  };
+
   return (
     <div className="col-md-4 mb-3">
       <div className="card">
@@ -11,9 +71,33 @@ const Receta = ({ receta }) => {
           alt={`Imagen de ${receta.strDrink}`}
         />
         <div className="card-body">
-          <button type="button" className="btn btn-primary btn-block">
+          <button
+            type="button"
+            className="btn btn-primary btn-block"
+            onClick={() => {
+              guardarIdReceta(receta.idDrink);
+              handleOpen();
+            }}
+          >
             Ver Receta
           </button>
+          <Modal
+            open={open}
+            onClose={() => {
+              handleClose();
+              guardarIdReceta(null);
+              guardarReceta({});
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h2>{inforeceta.strDrink}</h2>
+              <h3 className="mt-4">Instrucciones</h3>
+              <p>{inforeceta.strInstructions}</p>
+              <img className="img-fluid my-4" src={inforeceta.strDrinkThumb} />
+              <h3>Ingredientes y cantidades</h3>
+              <ul>{mostrarIngredientes(inforeceta)}</ul>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
